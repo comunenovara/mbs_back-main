bfebce7dc9c52c9b58f6b0fae60c1e2897a12df2
Cavallaro Catello Stefano <stefano@stepo.biz>
2024-04-18 18:58:12 +0200

    Initial configuration of generated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MbsMainModule } from './mbs-main/mbs-main.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MbsMainModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
